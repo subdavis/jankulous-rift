39f6999188a5875d3d595015eb36b9da2543a8ee
Brandon Davis <bdavis@redspin.net>
2016-04-09 04:42:55 -0400

    tracking 2-ball thingy

---
+++ tracking/tracking.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+int midpointX = -1;
+int midpointY = -1;
+
+int midpointXl = -1;
+int midpointYl = -1;
+
+
+struct v2{
+   int x;
+   int y;
+};
+
+v2 *red;
+v2 *green;
+v2 *midp;
+v2 *lastmid;
+
+int trackColor(v2 *ret, Mat imgLines, VideoCapture cap, int iLowH, int iHighH, int iLowS, int iHighS, int iLowV, int iHighV){
+
+    Mat imgOriginal;
+
+    bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+    if (!bSuccess){ //if not success, break loop{
+         cout << "Cannot read a frame from video stream" << endl;
+         //break;
+    }
+
+    Mat imgHSV; 
+
+    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+   
+    Mat imgThresholded;
+
+    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+        
+    //morphological opening (removes small objects from the foreground)
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+     //morphological closing (removes small holes from the foreground)
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+     //Calculate the moments of the thresholded image
+    Moments oMoments = moments(imgThresholded);
+
+    double dM01 = oMoments.m01;
+    double dM10 = oMoments.m10;
+    double dArea = oMoments.m00;
+
+     // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+    if (dArea > 10000){
+      //calculate the position of the ball
+      int posX = dM10 / dArea;
+      int posY = dM01 / dArea;
+
+     //TODO set RET values 
+     ret->x = posX;
+     ret->y = posY;     
+     midp->x = (green->x + red->x) / 2; 
+     midp->y = (green->y + red->y) / 2;
+
+      if (lastmid->x >= 0 && lastmid->y >= 0 && posX >= 0 && posY >= 0){
+        //Draw a red line from the previous point to the current point
+        line(imgLines, Point(midp->x, midp->y), Point(lastmid->x, lastmid->y), Scalar(0,0,255), 2);
+      }
+        //TODO - anything to do here?
+      lastmid->x = midp->x;
+      lastmid->y = midp->y;
+    }
+
+    imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+    imgOriginal = imgOriginal + imgLines;
+    imshow("Original", imgOriginal); //show the original image
+
+    if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+    {
+      cout << "esc key is pressed by user" << endl;
+      //break; 
+    }
+  return 0;
+}
+
+ int main( int argc, char** argv )
+ {
+
+  red = new v2();
+  red->x = 0;
+  red->y = 0;
+  green = new v2();
+  green->x = 0;
+  green->y = 0;
+  midp = new v2();
+  midp->x = 0;
+  midp->y = 0;
+  lastmid = new v2();
+  lastmid->x = 0;
+  lastmid->y = 0;
+
+    VideoCapture cap(0); //capture the video from webcam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+// For Red
+  int iLowRed = 170;
+ int iHighRed = 179;
+
+  int iLowSr = 150; 
+ int iHighSr = 255;
+
+  int iLowVr = 60;
+ int iHighVr = 255;
+
+//For Green
+  int iLowGreen = 73;
+ int iHighGreen = 108;
+
+   int iLowSg = 150; 
+ int iHighSg = 255;
+
+  int iLowVg = 26;
+ int iHighVg = 180;
+
+ // //  //Create trackbars in "Control" window
+ // createTrackbar("LowH", "Control", &iLowRed, 179); //Hue (0 - 179)
+ // createTrackbar("HighH", "Control", &iHighRed, 179);
+
+ //  createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+ // createTrackbar("HighS", "Control", &iHighS, 255);
+
+ //  createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+ // createTrackbar("HighV", "Control", &iHighV, 255);
+
+ while (true){
+    //Capture a temporary image from the camera
+  Mat imgTmp;
+  cap.read(imgTmp); 
+
+  //Create a black image with the size as the camera output
+  Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
+
+  trackColor(red, imgLines, cap, iLowRed, iHighRed, iLowSr, iHighSr, iLowVr, iHighVr);
+  trackColor(green, imgLines, cap, iLowGreen, iHighGreen, iLowSg, iHighSg, iLowVg, iHighVg);
+  
+  }
+  
+  return 0;
+}
